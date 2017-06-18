0b0b2f85fc4c10eee1891a626276cf96053b7e4e
Kristy Chen <kristychen118@gmail.com>
2017-06-18 14:46:19 -0400

    assignment 6 - fb email

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -8,7 +8,8 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var facebookConfig = {
     clientID : process.env.FACEBOOK_CLIENT_ID,
     clientSecret : process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL : process.env.FACEBOOK_CALLBACK_URL
+    callbackURL : process.env.FACEBOOK_CALLBACK_URL,
+    profileFields : ['id', 'displayName', 'emails']
 };
 
 passport.use(new LocalStrategy(localStrategy));

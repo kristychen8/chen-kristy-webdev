63b688e3d8d7d33ea525de4d75c4aae48e375b27
Kristy Chen <kristychen118@gmail.com>
2017-06-29 05:44:54 -0400

    Project

--- project/app.js
+++ project/app.js
@@ -7,9 +7,8 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionStringProject = 'mongodb://' + username + ':' + password;
-    connectionStringProject += '@ds137882.mlab.com:37882/heroku_x0bgswvw'; // user yours
+    connectionStringProject += '@ds153677.mlab.com:53677/heroku_hb4pgxdb'; // user yours
 }
-
 mongoose.connect(connectionStringProject);
 
 
